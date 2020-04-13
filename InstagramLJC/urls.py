26556196cb1646cf44000ea0314b1e2de4abfab9
Jiachen Li <jl5166@columbia.edu>
2020-04-12 23:02:47 -0400

    merge database and update admin

--- InstagramLJC/urls.py
+++ InstagramLJC/urls.py
@@ -16,4 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("insta/", include("insta.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), 
+            path("insta/", include("insta.urls")),
+            ]
--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+class Post(models.Model):
+    title = models.TextField(blank=True, null=True)
+    image = ProcessedImageField(
+        upload_to="static/images/posts",
+        format="JPEG",
+        options={"quality": 100},
+        blank=True,
+        null=True,
+    )
 
-# Create your models here.
